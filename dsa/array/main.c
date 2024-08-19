fe0b0c240a0d64ff2f1b7bbb30ffeac59e126949
Krushna <krushnabankar79@gmail.com>
2024-08-20 01:24:39 +0530

    array final changes

--- dsa/array/main.c
+++ dsa/array/main.c
@@ -6,13 +6,13 @@ enum {INIT, APPEND, DISPLAY, REMOVE, MAX, MIN, MERGE, INSERT, QUIT};
 
 int main() {
 	
-	array a;
-	array b;
+	array a, b;
+    char ch;
     int choice, index, num, size;
     while(1) {
         display_menu();
         scanf("%d", &choice);
-        switch(choice) {
+        switch(choice - 1) {
             case INIT:
                 printf("Enter size of array a: \n");
                 scanf("%d", &size);
@@ -22,8 +22,19 @@ int main() {
                 init(&b, size);
                 break;
             case APPEND:
-                append(&a, rand());
-                append(&b, rand());
+                printf("Enter array name (a or b): \n");
+                getchar();
+                ch = getchar();
+                switch (ch) {
+                    case 'a':
+                        append(&a, rand() % 100);
+                        break;
+                    case 'b':
+                        append(&b, rand() % 100);
+                        break;
+                    default:
+                        printf("Enter valid array: \n");
+                } 
                 break;
             case DISPLAY:
                 printf("Array a elements are: \n");
@@ -32,9 +43,23 @@ int main() {
                 display(b);
                 break;
             case REMOVE:
-                printf("Enter the index to remove element: \n");
-                scanf("%d", &index);
-                remove_at_index(&a, index);
+                printf("Enter array name (a or b): \n");
+                getchar();
+                ch = getchar();
+                switch (ch) {
+                    case 'a':
+                        printf("Enter the index to remove element: \n");
+                        scanf("%d", &index);
+                        remove_at_index(&a, index);
+                        break;
+                    case 'b':
+                        printf("Enter the index to remove element: \n");
+                        scanf("%d", &index);
+                        remove_at_index(&b, index);
+                        break;
+                    default:
+                        printf("Enter valid array: \n");
+                } 
                 break;
             case MAX:
                 printf("%d is the maximum element in the array a: \n", max(a));
@@ -45,15 +70,30 @@ int main() {
                 printf("%d is the minimum element in the array b: \n", min(b));
                 break;
             case MERGE:
-                printf("Array after merge of a and b: \n");
                 merge(&a, &b);
                 break;
             case INSERT:
-                printf("Enter element value and index to insert in array(element index): \n");
-                scanf("%d%d", &num, &index);
-                insert_at_index(&a, index, num);
-                printf("Array after insert operation: \n");
-                display(a);
+                printf("Enter array name (a or b): \n");
+                getchar();
+                ch = getchar();
+                switch (ch) {
+                    case 'a':
+                        printf("Enter element value and index to insert in array(element index): \n");
+                        scanf("%d%d", &num, &index);
+                        insert_at_index(&a, index, num);
+                        printf("Array after insert operation: \n");
+                        display(a);
+                        break;
+                    case 'b':
+                        printf("Enter element value and index to insert in array(element index): \n");
+                        scanf("%d%d", &num, &index);
+                        insert_at_index(&b, index, num);
+                        printf("Array after insert operation: \n");
+                        display(b);
+                        break;
+                    default:
+                        printf("Enter valid array: \n");
+                } 
                 break;
             case QUIT:
                 return 0;
